6330a0543b9832773390e2f7be6f057507068418
mannann <ninshar@pm.me>
2019-05-29 19:28:12 +0000

    Added insertAtEnd and increaseArraySize methods

--- CLang/Arrays/Methods/main.c
+++ CLang/Arrays/Methods/main.c
@@ -19,6 +19,20 @@ Array defineArray(){
     return arr;
 }
 
+// Increase Size
+
+void increaseSize(Array *arr){
+    int *p;
+    p = (int *)malloc(sizeof(int) * arr->size * 2);
+    arr->size = arr->size * 2;
+    for(int i = 0; i < arr->len; i++){
+        p[i] = arr->A[i];
+    }
+    free(arr->A);
+    arr->A = p;
+    p = NULL;
+}
+
 // Display Elements in Array.
 void displayArray(Array arr){
     for(int i = 0; i < arr.len; i++){
@@ -27,9 +41,32 @@ void displayArray(Array arr){
     printf("\n");
 }
 
+
+// Insert at end
+void insertAtEnd(Array *arr, int num){
+   if (arr->len == arr->size){
+      increaseSize(arr);
+   }
+   arr->A[arr->len++] = num;
+}
+
 int main(){
     printf("Array Declaration in C.\n");
     Array arr;
     arr = defineArray();
     displayArray(arr);
+    insertAtEnd(&arr, 11);
+    insertAtEnd(&arr, 12);
+    insertAtEnd(&arr, 13);
+    insertAtEnd(&arr, 14);
+    insertAtEnd(&arr, 15);
+    insertAtEnd(&arr, 16);
+    insertAtEnd(&arr, 17);
+    insertAtEnd(&arr, 18);
+    insertAtEnd(&arr, 19);
+    insertAtEnd(&arr, 20);
+    insertAtEnd(&arr, 21);
+    displayArray(arr);
+    printf("Len\t:%d \n", arr.len);
+    printf("Size\t:%d \n", arr.size);
 }
